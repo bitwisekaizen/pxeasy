9c22a271e37cd367278a325b0273f6e596291937
Gavin Gray <gavin@infinio.com>
2015-03-23 21:29:06 -0400

    Move some files around.

--- src/main/java/com/thegrayfiles/Application.java
+++ src/main/java/com/thegrayfiles/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 
 @ComponentScan
 @EnableAutoConfiguration
-@ImportResource(value = "classpath:integrationContext.xml")
+@ImportResource(value = "classpath:spring/integrationContext.xml")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
